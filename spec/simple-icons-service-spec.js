32436fdf9efa1d88a77a716c5888dc4231025a12
Yurii Habrusiev <yuriihabrusiev@gmail.com>
2017-02-02 03:01:12 +0200

    Fix code style with xo linter

--- spec/simple-icons-service-spec.js
+++ spec/simple-icons-service-spec.js
@@ -2,42 +2,42 @@
 
 import SimpleIconsService from '../lib/simple-icons-service';
 
-describe('SimpleIconsService', function() {
+describe('SimpleIconsService', () => {
   const fileIcons = new SimpleIconsService();
 
-  it('defaults to text', function() {
+  it('defaults to text', () => {
     expect(fileIcons.iconClassForPath('foo.bar')).toEqual('icon-file-text');
   });
 
-  it('recognizes .js code files', function() {
+  it('recognizes .js code files', () => {
     expect(fileIcons.iconClassForPath('foo.js')).toEqual('icon-file-code');
   });
 
-  it('recognizes .rb code files', function() {
+  it('recognizes .rb code files', () => {
     expect(fileIcons.iconClassForPath('foo.rb')).toEqual('icon-file-code');
   });
 
-  it('recognizes Makefile', function() {
+  it('recognizes Makefile', () => {
     expect(fileIcons.iconClassForPath('Makefile')).toEqual('icon-file-code');
   });
 
-  it('recognizes Markdown files', function() {
+  it('recognizes Markdown files', () => {
     expect(fileIcons.iconClassForPath('foo.md')).toEqual('icon-file-text');
   });
 
-  it('recognizes compressed files', function() {
+  it('recognizes compressed files', () => {
     expect(fileIcons.iconClassForPath('foo.zip')).toEqual('icon-file-zip');
   });
 
-  it('recognizes image files', function() {
+  it('recognizes image files', () => {
     expect(fileIcons.iconClassForPath('foo.png')).toEqual('icon-file-media');
   });
 
-  it('recognizes PDF files', function() {
+  it('recognizes PDF files', () => {
     expect(fileIcons.iconClassForPath('foo.pdf')).toEqual('icon-file-pdf');
   });
 
-  it('recognizes binary files', function() {
+  it('recognizes binary files', () => {
     expect(fileIcons.iconClassForPath('foo.exe')).toEqual('icon-file-binary');
   });
 });
